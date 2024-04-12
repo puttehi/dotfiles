6eca7dc9b4e626c0a8efdb5c02adf1d2b3824c27
puttehi <peteeez95@gmail.com>
2024-04-12 11:55:09 +0300

    Basic install scripts

---
+++ install-basic-tools.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo ""
+echo "Installing basic stuff... There are newer versions available outside of apt"
+echo ""
+echo "Check for prompts!"
+echo ""
+
+sudo apt update
+sudo apt install git python3 python3-venv curl wget ripgrep fd-find bat fzf tldr tree zoxide lolcat gcc clang golang make unzip
+
+echo ""
+echo "Oh My Posh (requires wget)"
+echo ""
+
+~/install-oh-my-posh.sh
+
+echo ""
+echo "Node Version Manager"
+echo ""
+
+~/install-nvm-v0.39.7.sh
+
--- install-oh-my-posh.sh
+++ install-oh-my-posh.sh
@@ -1,20 +1,27 @@
 #! /bin/bash
 _download_date=$(date -u +"%Y-%m-%d")
+
 # Download OMP
-sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+OMP_VERSION="v8.17.0"
+
+sudo wget "https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/$OMP_VERSION/posh-linux-amd64" -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
+
 # Download themes
-cp -r ~/.poshthemes ~/.poshthemes.$_download_date.bak
+cp -r ~/.poshthemes "$HOME/.poshthemes.$_download_date.bak"
 echo "Backup copied to ~/.poshthemes.$_download_date.bak"
 mkdir ~/.poshthemes -p
 _download_filename="themes_$_download_date.zip"
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/$_download_filename
-unzip -v ~/.poshthemes/$_download_filename -d ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O "$HOME/.poshthemes/$_download_filename"
+unzip -v "$HOME/.poshthemes/$_download_filename" -d ~/.poshthemes
 chmod u+rw ~/.poshthemes/*.json
-rm ~/.poshthemes/$_download_filename
+rm "$HOME/.poshthemes/$_download_filename"
+
 # Preview themes
-for file in ~/.poshthemes/*.omp.json; do echo "$file\n"; oh-my-posh --config $file; echo ""; done;
+for file in ~/.poshthemes/*.omp.json; do printf "%s\n" "$file"; oh-my-posh --config "$file"; echo ""; done;
+
 # Show enable guidance
 echo "*****************"
-echo "Add eval "$(printf '$(oh-my-posh init --config ~/.poshthemes/jandedobbeleer.omp.json)')" to your /.bashrc"
+# shellcheck disable=SC2016
+echo "Add eval \"$(printf '$(oh-my-posh init --config ~/.poshthemes/jandedobbeleer.omp.json)')\" to your /.bashrc"
 echo "*****************"
